048df8a163aeff6c3d5f74ca64b3d6d3702771d1
victoria_mali <153543182+victoria-mali@users.noreply.github.com>
2026-03-10 17:05:43 +0000

    Add basic calculator functions

---
+++ script.js
@@ -0,0 +1,29 @@
+const add = function(a, b) {
+    return a + b;
+};
+
+const subtract = function(a, b) {
+    return a - b;
+};
+
+const multiply = function(a, b) {
+    return a * b;
+};
+
+const divide = function(a, b) {
+    return a / b;
+};
+
+let num1, num2, operator;
+
+const operate = function(operator, num1, num2) {
+    if (operator === "+") {
+        add(num1, num2);
+    } else if (operator === "-") {
+        subtract(num1, num2);
+    } else if (operator === "*") {
+        multiply(num1, num2);
+    } else if (operator === "/") {
+        divide(num1, num2);
+    }
+};
